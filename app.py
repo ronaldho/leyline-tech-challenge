29f208290bf5374658cdea0999a8a27d0cd7eeb9
Ronald Ho <mail@ronaldho.me>
2024-07-05 14:37:43 -0700

    first draft with progress bar, uploads

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, redirect, url_for, request, render_template, Response, flash, session
+from time import sleep
+
+from rq import Queue
+from rq.job import Job
+from rq import get_current_job
+
+from redis import Redis
+
+import time
+import json
+import os
+
+r = Redis(os.environ.get('REDIS_URL','localhost'))
+q = Queue(connection=r)
+
+
+def ai_simulator(data):
+    job = get_current_job()
+    
+    n=0
+    top = 30
+    while n < top: 
+        job.meta['progress'] = n/top*100
+        job.save_meta()
+        time.sleep(1)
+        n=n+1
+    
+    data = data.upper()
+    return 'Processed %s' % (data,)
+
+app = Flask(__name__, template_folder='.')
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def upload():
+    print('in upload')
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            value = uploaded_file.filename
+            job = q.enqueue(ai_simulator, value)
+            print(job.id)
+            # return {'job_id': job.id }
+            flash(job.id)
+    return redirect(url_for('index')+'?job_id='+job.id)
+
+@app.route('/progress/<string:job_id>')
+def progress(job_id):
+    def get_status():
+        job = Job.fetch(job_id, connection=r)
+        status = job.get_status()
+        
+        while status != 'finished':
+
+            status = job.get_status()
+            job.refresh()
+
+            d = {'status': status}
+
+            if 'progress' in job.meta:
+                d['value'] = job.meta['progress']
+            else:
+                d['value'] = 0
+                
+            # IF there's a result, add this to the stream
+            if job.result:
+                d['result'] = job.result
+
+            json_data = json.dumps(d)
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+
+    return Response(get_status(), mimetype='text/event-stream')
